ac5509bf4dd8c793bc55d5f6413ca26deb06ca1c
xdoo3 <xd003_cz@proton.me>
2026-07-27 15:48:53 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		{
+			name: 'strip-ts-optional',
+			script: async ({ content, attributes }) => {
+				if (attributes.lang === 'ts') {
+					return {
+						code: content.replace(/(\w+)\?(\s*[:,)])/g, '$1$2')
+					};
+				}
+			}
+		}
+	],
 
 	kit: {
 		adapter: adapter({
